97d722a912606b11e7dc5220d74abc00c960070f
Keesha Hargrove <kharg14@gmail.com>
2020-10-03 15:29:12 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
+require 'sinatra/base'
+require 'pry'
 
+class App < Sinatra::Base
+
+    get '/newteam' do
+        erb :newteam
+    end
+
+    post '/team' do
+        erb :team
+    end
+
+end
 
 
 
